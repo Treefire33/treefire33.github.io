ac96008c14371636d1cc8b264f14261dab27eab0
Braidon <63606230+Treefire33@users.noreply.github.com>
2022-05-10 15:03:36 -0500

    Update 404.js

--- js/404.js
+++ js/404.js
@@ -1,10 +1,10 @@
 var timesClicked = 0;
-var button = document.getElementById("b").onclick = ClickThingie;
+document.getElementById("b").onclick = ClickThingie;
 function ClickThingie()
 {
   timesClicked+=1 
-}
-if(timesClicked==100)
-{
-  document.body.innerHTML='<p>Suprise! you get nothing!</p><p style="font-size: 12px">(You\'re still on a 404, go to the<a href="https://firetreegames.net">Home page</a>)</p>';
+  if(timesClicked==100)
+  {
+    document.body.innerHTML='<p>Suprise! you get nothing!</p><p style="font-size: 12px">(You\'re still on a 404, go to the<a href="https://firetreegames.net">Home page</a>)</p>';
+  }
 }
